8136387a7b0655d08484a42772cf61967a74e045
Md Anas Sabah <mdanassabah@gmail.com>
2023-01-27 22:12:51 +0530

    ADDED: helper.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,14 +3,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
-
-function filterData(searchText, restaurants) {
-  const filterData = restaurants.filter((restaurant) =>
-    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
-  );
-
-  return filterData;
-}
+import { filterData } from "../utils/helper";
 
 const Body = () => {
   const [allRestaurants, setAllRestaurants] = useState([]);
